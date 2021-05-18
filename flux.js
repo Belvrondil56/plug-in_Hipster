efbd027988583f5b87187a63c1abb77a9e86df1a
GuillaumeM-OPC <gu.martin@yahoo.com>
2021-05-18 15:33:21 +0100

    Added tabs to get list of newest and hottest stories

--- flux.js
+++ flux.js
@@ -1,30 +1,51 @@
-let content = document.getElementById("posts");
+let tabs = document.getElementsByClassName("tabs");
+let content = document.getElementsByClassName("content")[0];
 
 // get "/api/stories/tag/:tag" => "api/tags # find_stories_by_tags"
 // get "/api/stories" => "api/stories_api # find_all_stories"
 // http://127.0.0.1:3000/api/stories/tag/consequuntur
 
-fetch('http://127.0.0.1:3000/newest.json', {
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'plain/text;charset=UTF-8'
-    }
-})
-.then(response => response.json())
-.then(data => {
+for (var i = 0; i < tabs.length; i++) {
+    tabs.item(i).addEventListener("click", (e) => {
+  
+        let toFetch = e.currentTarget.getAttribute("id");
 
-    console.log(data)
+        console.log(toFetch)
+
+        let selected = document.getElementsByClassName("selected")[0];
+        selected.classList.remove("selected")
+
+        let btn = document.getElementById(toFetch); 
+        btn.classList.add("selected")
+
+        fetchFunc(toFetch)
+
+    })
+}
+
+function fetchFunc(toFetch) {
+    fetch('http://127.0.0.1:3000/'+toFetch+'.json', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'plain/text;charset=UTF-8'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
     
-    content.innerHTML = ""
-
-    data.forEach(element => {
-        content.innerHTML +=
-        `
-            <p> ${element["title"]} </p>
-        `         
+        console.log(data)
+        
+        content.innerHTML = ""
+    
+        data.forEach(element => {
+            content.innerHTML +=
+            `
+                <div> ${element["title"]} </div>
+            `
+        });
+    
+    })
+    .catch((error) => {
+        console.error('Error:', error);
     });
-
-})
-.catch((error) => {
-    console.error('Error:', error);
-});
+}
